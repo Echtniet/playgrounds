1eadc2da7e59efdf6c0da463f0de17519df12d81
clinton davelaar <davelaarprogramming@gmail.com>
2019-09-16 13:12:37 -0500

    just push

---
+++ Optionals and Closures.playground/Contents.swift
@@ -0,0 +1,62 @@
+import UIKit
+
+
+// Write one or more statements to accomplish each task:
+
+// 0. Create a String optional, name. Don't assign it a value.
+var name:String?
+
+// 1. Create a Double optional, stockPrice. Assign it the value 100.0
+var stockPrice:Double? = 100.0
+
+// 2. Double the value of stockPrice.
+stockPrice = 2 * stockPrice!
+
+// 3. Create an implicitly unwrapped optional, guess; assign it a random vbalue between 1-100
+var guess:Double! = Double.random(in: 1.0 ... 100.0)
+
+// 4. Increment guess's value by 50.0
+guess += 50.0
+
+
+
+var voltageTF = UITextField()
+voltageTF.text = "25.0"
+
+// 5. Write an if-let statement that will take the contents of voltageTF, and (if possible), store half its value back in the text field. If not possible, store "0.0" in voltageTF.
+if var volt = Double(voltageTF.text!){
+    volt /= 2
+    voltageTF.text = String(volt)
+}else{
+    voltageTF.text = String(0.0)
+}
+
+
+// 6. Write a closure with two parameters - an Int and an [Int] that will return the number of times the Int is found in the array.
+let inArray = {(x:Int, myArray:[Int]) -> Int in
+    var count = 0
+    for y in myArray{
+        if x == y {
+            count += 1
+        }
+    }
+    return count
+}
+
+
+// 7. Assign the closure to a variable, count, and then invoke it with the values 5 and [5, 10, 15, 5]
+var count = inArray(5, [5, 10, 15, 5])
+
+
+// 8. What is the *type* of the closure that you just created?
+//(Int, [Int]) -> Int
+
+// 9. Write a function quest9(demo:), where the parameter demo will be of type (Int, [Int]) -> Int
+
+func quest9(demo:(Int, [Int]) -> Int){
+    demo(5, [50, 250, 350])
+}
+
+// 10. Invoke quest9(demo:) with the closure that you wrote in 9. Inside quest9(demo:), print out the results of invoking demo with the values 5 and [50, 250, 350].
+
+quest9(demo: <#T##(Int, [Int]) -> Int#>)
